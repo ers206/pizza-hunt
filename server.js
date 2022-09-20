4480ad847ccaeb5e68a67ad5c275cec435470885
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-09-20 18:14:10 -0500

    deploying end of mod 18

--- server.js
+++ server.js
@@ -10,10 +10,19 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/Pizza-hunt',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
+// below was in mod 18======= above was from end of mod 18 to connect mogodb and heroku
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pizza-hunt', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
